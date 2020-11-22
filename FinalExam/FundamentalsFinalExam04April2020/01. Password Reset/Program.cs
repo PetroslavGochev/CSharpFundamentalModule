315816fe5a96c9fbf5e3f018d34bbc7123104009
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2020-11-22 18:29:01 +0200

    Practise

---
+++ FinalExam/FundamentalsFinalExam04April2020/01. Password Reset/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+
+namespace _01._Password_Reset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            StringBuilder sb = new StringBuilder(Console.ReadLine());
+            string input = string.Empty;
+            while((input = Console.ReadLine()) != "Done")
+            {
+                var data = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var command = data[0];
+                if (command == "TakeOdd") TakeOdd(sb);
+                else if (command == "Cut") Cut(sb, data);
+                else if (command == "Substitute") Substitute(sb, data);
+            }
+            Console.WriteLine($"Your password is: {sb}");
+        }
+        static void TakeOdd(StringBuilder sb)
+        {
+            for (int i = 0; i < sb.Length; i++)
+            {
+                sb.Remove(i,1);
+            }
+            Console.WriteLine(sb);
+        }
+        static void Cut(StringBuilder sb,string[] input)
+        {
+            int index = int.Parse(input[1]);
+            int length = int.Parse(input[2]);
+            sb.Remove(index, length);
+            Console.WriteLine(sb);  
+        }
+        static void Substitute(StringBuilder sb, string[] input)
+        {
+            string substring = input[1];
+            string substitute = input[2];
+            if (sb.ToString().Contains(substring))
+            {
+                sb.Replace(substring, substitute);
+                Console.WriteLine(sb);
+            }
+            else
+            {
+                Console.WriteLine("Nothing to replace!");
+            }
+        }
+    }
+}
---
+++ FinalExam/FundamentalsFinalExam04April2020/02. Fancy Barcodes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _02._Fancy_Barcodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            string pattern = @"\@[#]+[A-Z]+[A-Za-z\d]{4,}[A-Z]+\@[#]+";
+            string digit = @"[\d]+";
+            for (int i = 0; i < number; i++)
+            {
+                Match match = Regex.Match(Console.ReadLine(), pattern);
+                if(match.Length == 0)
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+                else
+                {
+                    MatchCollection group = Regex.Matches(match.ToString(), digit);
+                    if(group.Count == 0)
+                    {
+                        Console.WriteLine("Product group: 00");
+                        continue;
+                    }
+                    StringBuilder barcode = new StringBuilder();
+                    foreach (var dig in group)
+                    {
+                        barcode.Append(dig);
+                    }
+                    Console.WriteLine($"Product group: {barcode}");
+                }
+            }
+        }
+    }
+}
