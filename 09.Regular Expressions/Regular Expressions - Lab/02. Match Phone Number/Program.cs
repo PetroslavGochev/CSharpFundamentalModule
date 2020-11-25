8eb87f63c63c1720608d4de5dea079c47ba412c1
Petroslav <petroslavivovgochev1996@abv.bg>
2020-11-25 18:57:54 +0200

    Fixed

--- 09.Regular Expressions/Regular Expressions - Lab/02. Match Phone Number/Program.cs
+++ 09.Regular Expressions/Regular Expressions - Lab/02. Match Phone Number/Program.cs
@@ -9,7 +9,11 @@ namespace _02._Match_Phone_Number
     {
         static void Main(string[] args)
         {
-           
+            string pattern = @"\+359([\s|-])2\1[\d]{3}\1[\d]{4}\b";
+            Regex regex = new Regex(pattern);
+            string input = Console.ReadLine();
+            MatchCollection collectionOfPhoneNumbers = regex.Matches(input);
+            Console.WriteLine(string.Join(", ", collectionOfPhoneNumbers));
         }
     }
 }
