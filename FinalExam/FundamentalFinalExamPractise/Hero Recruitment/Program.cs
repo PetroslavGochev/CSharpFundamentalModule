54ad89b0b72d7df88ad652c548a473030d18b7f9
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2020-12-09 10:13:36 +0200

    Exam practise

---
+++ FinalExam/FundamentalFinalExamPractise/Hero Recruitment/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hero_Recruitment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> heroes = new Dictionary<string, List<string>>();
+            string input = string.Empty;
+            while((input = Console.ReadLine()) != "End")
+            {
+                var data = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var command = data[0];
+                var name = data[1];
+                if(command == "Enroll")
+                {
+                    Enroll(heroes, name);
+                }
+                else if (command == "Learn")
+                {
+                    var spellName = data[2];
+                    Learn(heroes, name, spellName);
+                }
+                else if (command == "Unlearn")
+                {
+                    var spellName = data[2];
+                    UnLearn(heroes, name, spellName);
+                }
+            }
+            PrintResult(heroes);
+        }
+        public static void Enroll(Dictionary<string, List<string>> heroes,string name)
+        {
+            if (heroes.ContainsKey(name))
+            {
+                Console.WriteLine($"{name} is already enrolled.");
+            }
+            else
+            {
+                heroes.Add(name, new List<string>());
+            }
+        }
+        public static void Learn(Dictionary<string, List<string>> heroes, string name, string spellName)
+        {
+            if (!heroes.ContainsKey(name))
+            {
+                Console.WriteLine($"{name} doesn't exist.");
+            }
+            else if (heroes[name].Contains(spellName))
+            {
+                Console.WriteLine($"{name} has already learnt {spellName}.");
+            }
+            else
+            {
+                heroes[name].Add(spellName);
+            }
+            
+
+        }
+        public static void UnLearn(Dictionary<string, List<string>> heroes,string name,string spellName)
+        {
+            if (!heroes.ContainsKey(name))
+            {
+                Console.WriteLine($"{name} doesn't exist.");
+            }
+            else if (heroes[name].Contains(spellName))
+            {
+                heroes[name].Remove(spellName);
+            }
+            else
+            {
+                Console.WriteLine($"{name} doesn't know {spellName}.");
+            }
+        }
+        public static void PrintResult(Dictionary<string, List<string>> heroes)
+        {
+            Console.WriteLine($"Heroes:");
+            foreach (var item in heroes.OrderByDescending(x=>x.Value.Count).ThenBy(x=>x.Key))
+            {
+                Console.WriteLine($"== {item.Key}: {string.Join(", ", item.Value)}");
+            }
+        }
+    }
+}
---
+++ FinalExam/FundamentalFinalExamPractise/Message Translator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Message_Translator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"!(?<command>[A-z][a-z]{2,})!:\[(?<message>[A-Za-z]{8,})]";
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                Match match = Regex.Match(Console.ReadLine(), pattern);
+                if(match.Length == 0)
+                {
+                    Console.WriteLine("The message is invalid");
+                    continue;
+                }
+                Console.Write($"{match.Groups["command"].Value}: ");
+                foreach (var character in match.Groups["message"].Value)
+                {
+                    Console.Write($"{(int)character} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
