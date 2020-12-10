c577de110796be0aed7e896aa21d8c6b87516728
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2020-12-10 11:30:37 +0200

    Exam practise

---
+++ FinalExam/FundamentalFinalExamPractise/Password/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(.+)\>(?<first>[\d]{3})\|(?<second>[a-z]{3})\|(?<third>[A-Z]{3})\|(?<fourth>[^\<\>]{3})\<\1";
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= number; i++)
+            {
+                StringBuilder sb = new StringBuilder();
+                Match match = Regex.Match(Console.ReadLine(), pattern);
+                if(match.Length == 0)
+                {
+                    Console.WriteLine("Try another password!");
+                    continue;
+                }
+                sb.Append(match.Groups["first"].Value);
+                sb.Append(match.Groups["second"].Value);
+                sb.Append(match.Groups["third"].Value);
+                sb.Append(match.Groups["fourth"].Value);
+                Console.WriteLine($"Password: {sb}");
+            }
+        }
+    }
+}
