7e40d565fd1468128d66b43bcb8b2a2413656466
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2020-12-11 10:06:53 +0200

    Exam practise

---
+++ FinalExam/FundamentalFinalExamPractise/Inbox Manager/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Inbox_Manager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> inbox = new Dictionary<string, List<string>>();
+            string input = string.Empty;
+            while((input = Console.ReadLine()) != "Statistics")
+            {
+                var data = input
+                    .Split("->", StringSplitOptions.RemoveEmptyEntries);
+                var command = data[0];
+                var username = data[1];
+                if (command == "Add") Add(inbox, username);
+                else if (command == "Delete") Delete(inbox, username);
+                else if(command == "Send")
+                {
+                    var email = data[2];
+                    Send(inbox, username, email);
+                }
+            }
+            PrintResult(inbox);
+        }
+        static void Add(Dictionary<string, List<string>> inbox,string username)
+        {
+            if (inbox.ContainsKey(username))
+            {
+                Console.WriteLine($"{username} is already registered");
+            }
+            else
+            {
+                inbox.Add(username, new List<string>());
+            }
+        }
+        static void Send(Dictionary<string, List<string>> inbox, string username,string email)
+        {
+            if (inbox.ContainsKey(username))
+            {
+                inbox[username].Add(email);
+            }
+        }
+        static void Delete(Dictionary<string, List<string>> inbox, string username)
+        {
+            if (inbox.ContainsKey(username))
+            {
+                inbox.Remove(username);
+            }
+            else
+            {
+                Console.WriteLine($"{username} not found!");
+            }
+        }
+        static void PrintResult(Dictionary<string, List<string>> inbox)
+        {
+            Console.WriteLine($"Users count: {inbox.Count}");
+            foreach (var item in inbox.OrderByDescending(x=>x.Value.Count).ThenBy(x=>x.Key))
+            {
+                Console.WriteLine($"{item.Key}");
+                foreach (var message in item.Value)
+                {
+                    Console.WriteLine($"- {message}");
+                }
+            }
+        }
+    }
+}
---
+++ FinalExam/FundamentalFinalExamPractise/On the Way to Annapurna/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace On_the_Way_to_Annapurna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> stores = new Dictionary<string, List<string>>();
+            string input = string.Empty;
+            while((input = Console.ReadLine()) != "END")
+            {
+                var data = input
+                    .Split("->", StringSplitOptions.RemoveEmptyEntries);
+                var command = data[0];
+                if (command == "Add" && data[2].Contains(",")) AddList(stores, data);
+                else if (command == "Add") Add(stores, data);
+                else if (command == "Remove") Remove(stores, data);         
+            }
+            PrintResult(stores);
+        }
+        static void Add(Dictionary<string, List<string>> stores,string[] data)
+        {
+            var store = data[1];
+            var item = data[2];          
+            if (!stores.ContainsKey(store))
+            {
+                stores.Add(store, new List<string>());
+            }
+            stores[store].Add(item);         
+        }
+        static void AddList(Dictionary<string, List<string>> stores, string[] data)
+        {
+            var store = data[1];
+            var listOfItem = data[2]
+                .Split(",", StringSplitOptions.RemoveEmptyEntries);
+            if (!stores.ContainsKey(store))
+            {
+                stores.Add(store, new List<string>());
+            }
+            foreach (var item in listOfItem)
+            {
+                stores[store].Add(item);
+            }
+        }
+        static void Remove(Dictionary<string, List<string>> stores, string[] data)
+        {
+            var store = data[1];
+            if (stores.ContainsKey(store))
+            {
+                stores.Remove(store);
+            }
+        }
+        static void PrintResult(Dictionary<string, List<string>> stores)
+        {
+            Console.WriteLine($"Store list:");
+            foreach (var item in stores.OrderByDescending(x=>x.Value.Count).ThenByDescending(x=>x.Key))
+            {
+                Console.WriteLine($"{item.Key}");
+                foreach (var it in item.Value)
+                {
+                    Console.WriteLine($"<<{it}>>");
+                }
+            }
+        }
+    }
+}
