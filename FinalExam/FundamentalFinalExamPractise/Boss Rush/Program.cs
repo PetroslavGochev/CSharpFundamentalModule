bc652f52074c06ef7490b9c8de0707fb0834666a
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2020-12-12 08:57:44 +0200

    Exam practise

---
+++ FinalExam/FundamentalFinalExamPractise/Boss Rush/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Boss_Rush
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\|(?<boss>[A-Z]{4,})\|:#(?<title>[A-Za-z]+\s[A-Za-z]+)#";
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                Match match = Regex.Match(Console.ReadLine(), pattern);
+                if (match.Length == 0)
+                {
+                    Console.WriteLine("Access denied!");
+                }
+                else
+                {
+                    string name = match.Groups["boss"].Value;
+                    string title = match.Groups["title"].Value;
+                    Console.WriteLine($"{name}, The {title}");
+                    Console.WriteLine($">> Strength: {name.Length}");
+                    Console.WriteLine($">> Armour: {title.Length}");
+                }
+            }
+        }
+    }
+}
---
+++ FinalExam/FundamentalFinalExamPractise/Mirror words/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Mirror_words
+{
+    class Program
+    {
+        public static object Matches { get; private set; }
+
+        static void Main(string[] args)
+        {
+            string pattern = @"(\@|#)(?<first>[A-Za-z]{3,})\1\1(?<second>[A-Za-z]{3,})\1";
+            string text = Console.ReadLine();
+            MatchCollection match = Regex.Matches(text, pattern);
+            List<Match> mirror = new List<Match>();
+            if(match.Count == 0)
+            {
+                Console.WriteLine("No word pairs found!");
+                return;
+            }
+            else
+            {
+                Console.WriteLine($"{match.Count} word pairs found!");
+                for (int i = 0; i < match.Count; i++)
+                {
+                    string first = match[i].Groups["first"].Value;
+                    string second = match[i].Groups["second"].Value;
+                    char[] secondChar = second.ToCharArray();
+                    Array.Reverse(secondChar);
+                    new String(secondChar);
+                    if(new string(secondChar) == first)
+                    {
+                        mirror.Add(match[i]);
+                    }            
+                }
+                if(mirror.Count != 0)
+                {
+                    Console.WriteLine("The mirror words are: ");
+                    for (int i = 0; i < mirror.Count; i++)
+                    {
+                        if(i == mirror.Count - 1)
+                        {
+                            Console.Write($"{mirror[i].Groups["first"].Value} <=> { mirror[i].Groups["second"].Value}");
+                            break;
+                        }
+                        Console.Write($"{mirror[i].Groups["first"].Value} <=> {mirror[i].Groups["second"].Value}, ");
+                    }
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("No mirror words!");
+                }
+            }
+        }
+    }
+}
